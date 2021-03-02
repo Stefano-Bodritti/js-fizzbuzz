48c12f4da7f8319854e600fc85f517041436b37b
Stefano <tepo@hotmail.it>
2021-03-02 15:23:55 +0100

    Esercizio completato

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,17 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 // 1. ciclo for che vada da 1 a 100
 for (var i = 1; i <= 100; i++) {
 
+  // 2. se multiplo di 3 e 5 stampo FizzBuzz
+  // 2a. se multiplo di 3 stampo FizzBuzz
+  // 2b. se multiplo di 5 stampo Buzz
+  // 2c. altrimenti stampo il numero
+  if ( (i % 3 == 0) && (i % 5 == 0) ) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
 }
